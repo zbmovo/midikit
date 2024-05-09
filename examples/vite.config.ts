abf18c80c4b3d8cddbcad789eea0e56870851d85
zbmovo <zbmovo@foxmail.com>
2024-05-09 17:08:25 +0800

    chore: 修改例子项目中的 vite 配置

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/midikit",
   resolve: {
     alias: {
       "@": path.join(import.meta.dirname, "src"),
